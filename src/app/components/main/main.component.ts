5df0891b1988bd783d68b3fe91295fbf6eb16865
Luis Quiroz Schlemm <95149148+lxxs13@users.noreply.github.com>
2024-03-14 11:01:59 -0600

    Cartas de presentación, ceración de componentes

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderModule } from '../../shared/header/header.module';
 import { PresentationCardComponent } from '../presentation-card/presentation-card.component';
 import { ProjectsSectionComponent } from '../projects-section/projects-section.component';
+import { ContactMeComponent } from '../contact-me/contact-me.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, HeaderModule, PresentationCardComponent, ProjectsSectionComponent],
+  imports: [CommonModule, HeaderModule, PresentationCardComponent, ProjectsSectionComponent, ContactMeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
