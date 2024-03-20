4dc823557676bc55e87e62f4a560aa6a225f0534
Luis Quiroz Schlemm <95149148+lxxs13@users.noreply.github.com>
2024-03-19 18:50:57 -0600

    Animaciones, cartas, imagenes, PDF

--- src/app/components/contact-me/contact-me.component.ts
+++ src/app/components/contact-me/contact-me.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,8 +6,47 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './contact-me.component.html',
-  styleUrl: './contact-me.component.css'
+  styleUrls: ['./contact-me.component.css']
 })
-export class ContactMeComponent {
+export class ContactMeComponent implements OnInit, AfterViewInit {
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    // Opciones para el IntersectionObserver
+    const options: IntersectionObserverInit = {
+      root: null, // El viewport del navegador es el contenedor
+      rootMargin: '0px', // No se aplica margen adicional
+      threshold: 1 // Cuando al menos el 50% del elemento está en la vista
+    };
+
+    // Crear un nuevo IntersectionObserver
+    const observer = new IntersectionObserver(this.handleIntersection, options);
+
+    // Elemento que deseas observar
+    const miComponente = document.querySelector('#form-main-contact');
+
+    // Observar el elemento
+    if (miComponente) {
+      observer.observe(miComponente);
+    }
+  }
+
+  // Método para manejar la entrada en la vista
+  private handleIntersection(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Si el componente está en la vista, aplicar la animación
+        entry.target.classList.add('animate__animated', 'animate__headShake');
+        // Una vez que se ha aplicado la animación, ya no necesitamos observar el componente
+        observer.unobserve(entry.target);
+      }
+    });
+
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -4,11 +4,12 @@ import { HeaderModule } from '../../shared/header/header.module';
 import { PresentationCardComponent } from '../presentation-card/presentation-card.component';
 import { ProjectsSectionComponent } from '../projects-section/projects-section.component';
 import { ContactMeComponent } from '../contact-me/contact-me.component';
+import { AboutMeComponent } from '../about-me/about-me.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, HeaderModule, PresentationCardComponent, ProjectsSectionComponent, ContactMeComponent],
+  imports: [CommonModule, HeaderModule, PresentationCardComponent, ProjectsSectionComponent, ContactMeComponent, AboutMeComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
