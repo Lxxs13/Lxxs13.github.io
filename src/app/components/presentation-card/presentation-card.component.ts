04ee7c39445430e7dd2981f0dd0517bfe8616836
lxxs13 <luis.quiroz90@gmail.com>
2024-04-11 18:07:51 -0600

    Libreria para animar texto, cambios en carta de presentación

--- src/app/components/presentation-card/presentation-card.component.ts
+++ src/app/components/presentation-card/presentation-card.component.ts
@@ -2,6 +2,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-presentation-card',
@@ -16,11 +17,21 @@ export class PresentationCardComponent implements OnInit {
   constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
+    let typed = new Typed('#expert', {
+      strings: ["web apps", "proyectos frontend", "proyectos backend", "diseño UX/UI"],
+      typeSpeed: 50,
+      startDelay: 625,
+      backSpeed: 100,
+      loop: true,
+    });
+
     this.breakpointObserver
       .observe([Breakpoints.Handset])
       .subscribe((result) => {
         this.isHandset = result.matches;
       });
   }
+
+
 
 }
